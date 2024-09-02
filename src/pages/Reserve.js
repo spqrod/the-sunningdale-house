4f3eb126c52d9430c56c39ce5bd08d015b39974c
rodionion <rodionvh@gmail.com>
2024-09-02 17:37:23 -0300

    Minor changes

---
+++ src/pages/Reserve.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Reserve() {
+  return (
+    <main className="reserve">
+
+      <section className="reservationForm">
+        <h1>Reservation Form</h1>
+        <div className="form placeholder">
+            <p>Reservation form with dates and room types</p>
+        </div>
+      </section>
+
+      <section className="reservationRules">
+        <h2>Reservation Rules</h2>
+        <div className="form placeholder">
+            <p>Reservation rules info</p>
+        </div>
+      </section>
+
+    </main>
+  );
+}
+
+export default Reserve;
